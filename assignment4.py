d2db829b5fbde948245aa9a699f7fb7922c23d2c
LORDPHISSY <LORDPHISSY>
2017-11-17 12:13:15 +0000

    first code

---
+++ assignment4.py
@@ -0,0 +1,13 @@
+" The speeding ticket fine policy in Garki is N500 plus N500 for each mph over"
+"the limit plus a penalty of N200 for any speed over 90mph. write a program"
+"that accepts a speed limit and a clocked speed and either prints a message indicating"
+"the speed was legal or prints the amount of the fine, if the speed is illegal"
+print " Ticket policy in Garki is N500 plus N500 for each mph over the limit"
+limit = 100
+if (limit >= 100):
+    print " your speed is illegal"
+elif (limit <= 100):
+        print  "your speed is legal"
+else:
+    print " invalid entry"
+            
